016de1aef7cff5a81fa1d11821f86ba088e41909
ArmenHagopian <14040@ecam.be>
2016-02-03 14:37:33 +0100

    Second commit

--- test_utils.py
+++ test_utils.py
@@ -7,15 +7,24 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
+        with self.assertRaises(ValueError):
+            utils.fact(-1)
+        self.assertEqual(utils.fact(0), 1)
+        self.assertEqual(utils.fact(1), 1)
+        self.assertEqual(utils.fact(2), 2)
         # À compléter...
         pass
     
     def test_roots(self):
+        self.assertEqual(utils.roots(0, 0, 0), (0))
+        self.assertEqual(utils.roots(1, 0, 1), ())
+
         # À compléter...
         pass
     
     def test_integrate(self):
         # À compléter...
+        self.assertEqual(utils.integrate('x ** 2 - 1', -1, 1), -4.0/3)
         pass
 
 if __name__ == '__main__':
